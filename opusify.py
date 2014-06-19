e2f9661a5701c1aa38ab814a8a9c0c58746306f8
Russell Haley <rhaley@lily>
2014-06-19 00:18:56 -0500

    Add AGPL license headers, remove debugging tweaks.

--- opusify.py
+++ opusify.py
@@ -1,5 +1,21 @@
 #!/usr/bin/env python2
 
+#    Program to prepare audio files for portable devices.
+#    Copyright (C) 2014  Russell Haley
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 """
 Program for preparing audio files for portable devices. Transcodes to opus 
 and deletse the original, if the original is lossless or high enough quality
